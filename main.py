57b08023a39f80a30afed5c0fc29d95e5d243fc4
Dinesh shinde <dineshshinde9021@gmail.com>
2025-07-29 00:09:38 +0530

    main file modified

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from Project_OCR.extraction_file import ExtractPanDetails
 import config
 
 if __name__ == "__main__":
-    obj = ExtractPanDetails(config.file_path,config.pytesseract_path)
     result = obj.get_pan_details()
     print(result)
-
+# added new file
